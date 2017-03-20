284ea6ab0d3d208ce905d45bc5bdc309b40877a1
bobbykhanna <iceboxbob@gmail.com>
2017-03-19 22:49:34 -0400

    commit

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,9 @@
-$(function() {
-
-  // your code will go here
-
-});
+$.ajax({
+    url: 'https://codeschool.pluralsight.com/users/2516180.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+    var list =  response.courses.completed;
+    console.log(list[0].badges);
+    }
+  });
